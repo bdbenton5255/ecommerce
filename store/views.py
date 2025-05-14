2a808af0dfd3ae0610f93a57839ce85ab575ac36
Brandon Benton <brandon.benton.5255@gmail.com>
2025-05-14 16:25:15 -0500

    guest order processing

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ import json
 import datetime
 
 from .models import *
-from . utils import cookieCart, cartData
+from . utils import cookieCart, cartData, guestOrder
 
 def store(request):
 
@@ -69,24 +69,26 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transation_id
-
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                address=data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
-            )
 
     else:
-        print('User is not logged in.')
+        customer, order = guestOrder(request, data)
+
+    total = float(data['form']['total'])
+    order.transaction_id = transation_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
+
 
     return JsonResponse('Payment complete!', safe=False)
